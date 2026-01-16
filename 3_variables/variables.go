ca5080a82cfbd36a9dd7d88c8c00e237832bd60a
Chayan Koley <60001227+ChayanKoley153@users.noreply.github.com>
2026-01-16 21:54:09 +0530

    update commit

--- 3_variables/variables.go
+++ 3_variables/variables.go
@@ -15,9 +15,9 @@ func main() {
 	// var name string
 	// name = "golang"
 
-	// var price float32 = 50.5
-	// var price = 50.5
-	// price := 50.5
+	var price float32 = 50.5
+	var price = 50.5
+	price := 50.5
 
 	// fmt.Println(price)
 }
